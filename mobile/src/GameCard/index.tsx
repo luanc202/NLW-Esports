5a45c3a88bd40b801c20100437371619981245f8
Luan <luan@luan-desktop>
2022-09-19 18:45:36 -0300

    Code updated until class 4

--- mobile/src/GameCard/index.tsx
+++ mobile/src/GameCard/index.tsx
@@ -4,12 +4,15 @@ import { THEME } from '../theme';
 import { Text } from 'react-native';
 
 import { styles } from './styles';
+import { useState } from 'react';
 
 export interface GameCardProps {
     id: string;
-    name: string;
-    ads: string;
-    cover: ImageSourcePropType
+    title: string;
+    _count: {
+        ads: string;
+    };
+    bannerUrl: string;
 }
 
 interface Props extends TouchableOpacityProps {
@@ -21,7 +24,7 @@ export function GameCard({ data, ...rest }: Props) {
         <TouchableOpacity style={styles.container} {...rest}>
             <ImageBackground
                 style={styles.cover}
-                source={data.cover}
+                source={{uri: data.bannerUrl}}
             >
 
                 <LinearGradient
@@ -29,11 +32,11 @@ export function GameCard({ data, ...rest }: Props) {
                     style={styles.footer}
                 >
                     <Text style={styles.name}>
-                        {data.name}
+                        {data.title}
                     </Text>
 
                     <Text style={styles.ads}>
-                        {data.ads} anúncios
+                        {data._count.ads} anúncios
                     </Text>
 
 
